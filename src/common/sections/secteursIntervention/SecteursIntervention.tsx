0fa3c2d2aaead1ecf5b6648f8422bf4c5f07dd78
quentin marc <quentinmarc81@gmail.com>
2025-09-12 11:33:20 +0200

    ui: reprise carte interventions + responsive

--- src/common/sections/secteursIntervention/SecteursIntervention.tsx
+++ src/common/sections/secteursIntervention/SecteursIntervention.tsx
@@ -20,10 +20,71 @@ export default function PresentationGenerale() {
             <div className={styles.colonneTextes}>
                 <h2>{t("secteursIntervention.titre")}</h2>
                 <p>{t("secteursIntervention.texte1")}</p>
+                <div className={styles.containerTags}>
+                    <div className={styles.tag}>
+                        <span className={styles.tagColor}></span>
+                        <span className={styles.tagTexte}>
+                            {t("menu.TravauxPublics")}
+                        </span>
+                    </div>
+                    <div className={styles.tag}>
+                        <span className={styles.tagColor}></span>
+                        <span className={styles.tagTexte}>
+                            {t("menu.Desamiantage")}
+                        </span>
+                    </div>
+                    <div className={styles.tag}>
+                        <span className={styles.tagColor}></span>
+                        <span className={styles.tagTexte}>
+                            {t("menu.Demolition")}
+                        </span>
+                    </div>
+                </div>
+
                 <ul>
-                    {departements.map((dep, index) => (
-                        <li key={index}>{dep}</li>
-                    ))}
+                    <li>
+                        <span className={styles.departement}>
+                            Pyrénées Orientales (66)
+                        </span>
+                        <span className={styles.colorTP}></span>
+                        <span className={styles.colorAmiante}></span>
+                        <span className={styles.colorDemol}></span>
+                    </li>
+                    <li>
+                        <span className={styles.departement}>Aude (11)</span>
+                        <span className={styles.colorAmiante}></span>
+                        <span className={styles.colorDemol}></span>
+                    </li>
+                    <li>
+                        <span className={styles.departement}>Ariège (09)</span>
+                        <span className={styles.colorAmiante}></span>
+                        <span className={styles.colorDemol}></span>
+                    </li>
+                    <li>
+                        <span className={styles.departement}>Hérault (34)</span>
+                        <span className={styles.colorAmiante}></span>
+                    </li>
+                    <li>
+                        <span className={styles.departement}>Gard (30)</span>
+                        <span className={styles.colorAmiante}></span>
+                    </li>
+                    <li>
+                        <span className={styles.departement}>Tarn (81)</span>
+                        <span className={styles.colorAmiante}></span>
+                    </li>
+
+                    <li>
+                        <span className={styles.departement}>
+                            Haute Garonne (31)
+                        </span>
+                        <span className={styles.colorAmiante}></span>
+                    </li>
+                    <li>
+                        <span className={styles.departement}>
+                            +7 {t("secteursIntervention.autres")}
+                        </span>
+                        <span className={styles.colorAmiante}></span>
+                    </li>
                 </ul>
             </div>
         </section>
